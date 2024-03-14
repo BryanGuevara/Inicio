5d5097f05f783dbc9e5e21323f58fd5a80b28d45
Bagg 12 <bryanalbertoguevaraguerrero@gmail.com>
2024-03-14 13:48:33 -0600

    optimizacion del css, adicion de las IA, adicion de nuevos sitios en Jugar

--- js/frase.js
+++ js/frase.js
@@ -9,9 +9,7 @@ function mostrarFraseAleatoria() {
       if (Array.isArray(frases) && frases.length > 0) {
         var fraseAleatoria = frases[Math.floor(Math.random() * frases.length)];
         document.getElementById("frase").innerHTML = fraseAleatoria;
-      } else {
-        console.error('El array de frases no está definido o está vacío.');
-      }
+      } 
     })
     .catch(error => console.error('Error al cargar el archivo JSON:', error));
 }
